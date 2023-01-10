f4c68ad53fe7e0cd75adf3a7b84707b760741aad
pushparajHE <122092001+pushparajHE@users.noreply.github.com>
2023-01-10 23:40:33 +0530

    Update He.py

--- He.py
+++ He.py
@@ -1,7 +1,7 @@
 import CGM_DASH
 import streamlit as st
 import movement_dash
-import base64
+
 
 
     
@@ -20,21 +20,6 @@ page.app()
 
 
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
 
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.sidebar.markdown(page_bg_img, unsafe_allow_html=True)
     
-#set_background("HE_BACK.png")
+set_background("HE_BACK.png")
